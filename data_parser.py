51aa675120967f110930d469c6a97960949fb45a
Myles Scholz <Myles@Cascadias-iMac.local>
2018-12-20 10:22:10 -0800

    List of tuples from database

--- data_parser.py
+++ data_parser.py
@@ -1,15 +1,23 @@
 #Data Parser
 #Myles Scholz
 
+import re
+
 f = open("words.txt")
 words = []
+tuples = []
+
 for x in f:
 	x = x.lower()
 	x = x.replace("\n","%")
+	x = re.sub("[^a-zA-Z\-%]+","'",x)
 	if "'" not in x:
 		words.append(x)
 
 for x in range(0,len(words)):
 	words[x] = list(words[x])
 
-print(words)
+for x in range(0,len(words)):
+	tuples.append(list(zip(words[x][:], words[x][1:])))
+
+print(tuples)
